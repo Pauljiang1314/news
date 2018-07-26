4464e378d3232f40dab5a4b1843d47a038a7fea8
jack <jack@163.com>
2018-07-26 14:34:30 +0800

    123

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,30 @@
+//渲染用户登陆页的方法
 exports.showDignin = (reg, res) => {
     res.render('signin.html');
-    
+
+};
+//
+exports.handleSignin = (req, res) => {
+    const body = req.body;
+
+    const sql = 'SELECT * FROM `users` WHERE `email` = ?'
+
+    if(err) {
+        return res.send({})
+    };
+
+
+
+
+
+
+
+
+
+
+
+    //邮箱正确，验证密码
+
+    const sql = 'SELECT * FROM'
+
 };
